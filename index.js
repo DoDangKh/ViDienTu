7ab2e3dddc42c09384f929e4eb678c9e79b1bbdf
Ly2903 <tttrucly2903@gmail.com>
2023-03-25 14:31:29 +0700

    update vercel

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const initAPIRoute = require("./src/routes/api");
 const app = express();
-const port = 5000;
+const port = 7000;
 const route = require("./src/routes/api");
 const bp = require("body-parser");
 //cors
